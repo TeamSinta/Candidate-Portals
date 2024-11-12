c77d5a474f27c33c8627a186d072d2af14e76afb
Wubaboo <evan4217@gmail.com>
2024-11-12 17:16:00 -0500

    [feature] Saving and previewing the content

--- src/app/(app)/(user)/editor/content/[sectionId]/_components/content-editor-page-buttons.tsx
+++ src/app/(app)/(user)/editor/content/[sectionId]/_components/content-editor-page-buttons.tsx
@@ -2,11 +2,22 @@
 import { Button } from "@/components/ui/button";
 import React from "react";
 
-function ContentEditorPageButtons() {
+type Props = {
+    onSave: () => void;
+    onTogglePreview: () => void;
+    isPreviewing: boolean;
+};
+function ContentEditorPageButtons({
+    onSave,
+    onTogglePreview,
+    isPreviewing,
+}: Props) {
     return (
         <div className="flex items-center space-x-2">
-            <Button variant={"secondary"}>Preview</Button>
-            <Button>Save</Button>
+            <Button variant={"secondary"} onClick={onTogglePreview}>
+                {isPreviewing ? "Edit" : "Preview"}
+            </Button>
+            <Button onClick={onSave}>Save</Button>
         </div>
     );
 }
--- src/app/(app)/(user)/editor/content/[sectionId]/_components/editor-wrapper.tsx
+++ src/app/(app)/(user)/editor/content/[sectionId]/_components/editor-wrapper.tsx
@@ -1,33 +1,72 @@
 "use client";
 import { AppPageShell } from "@/app/(app)/_components/page-shell";
 import Editor from "@/components/editor";
-import { PortalSelect, SectionSelect } from "@/server/db/schema";
+import {
+    PortalSelect,
+    SectionContentType,
+    SectionSelect,
+} from "@/server/db/schema";
 import { YooptaContentValue } from "@yoopta/editor";
-import React from "react";
+import React, { useState } from "react";
 import ContentEditorPageButtons from "./content-editor-page-buttons";
+import { updateSectionContent } from "@/server/actions/portal/mutations";
+import { toast } from "sonner";
+import { useSidebar } from "@/components/ui/sidebar";
+import { Router } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 interface Props {
     section: SectionSelect;
     portal: PortalSelect;
 }
 function EditorWrapper({ section, portal }: Props) {
+    const [sectionContent, setSectionContent] = useState<YooptaContentValue>(
+        section.content as YooptaContentValue,
+    );
+    const [isPreviewing, setIsPreviewing] = useState<boolean>(false);
+    const sidebar = useSidebar();
+    const router = useRouter();
+    async function handleSave() {
+        try {
+            await updateSectionContent({
+                id: section.id,
+                portalId: portal.id,
+                title: section.title ?? "",
+                content: sectionContent,
+                contentType: SectionContentType.YOOPTA,
+                index: section.index,
+            });
+            toast.success("Section saved successfully");
+            router.push(`/editor/${portal.id}`);
+        } catch {
+            toast.error("Failed to save section");
+        }
+    }
+
+    function handleTogglePreview() {
+        sidebar.setOpen(isPreviewing);
+        setIsPreviewing(!isPreviewing);
+    }
+
     return (
         <AppPageShell
             title={`${portal.title}  >  ${section.title}`}
             description="Edit the contents of your portal here"
-            buttons={[<ContentEditorPageButtons key={0} />]}
+            buttons={[
+                <ContentEditorPageButtons
+                    key={0}
+                    onSave={handleSave}
+                    onTogglePreview={handleTogglePreview}
+                    isPreviewing={isPreviewing}
+                />,
+            ]}
         >
             <div className="container">
-                {" "}
-                Content Editor Page {section.id}
-                <div>{JSON.stringify(section)}</div>
-                <div>{JSON.stringify(portal)}</div>
                 <Editor
-                    content={section.content as YooptaContentValue}
-                    editable={true}
-                    onChange={() => {
-                        return;
-                    }}
+                    // key={isPreviewing ? "preview" : "edit"}
+                    content={sectionContent}
+                    editable={!isPreviewing}
+                    onChange={setSectionContent}
                 />
             </div>
         </AppPageShell>
