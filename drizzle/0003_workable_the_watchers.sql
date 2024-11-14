570a47a51e4b5126b6e1da64daaf67ce067e559c
msinta <shegowmo@gmail.com>
2024-11-14 15:42:46 -0500

    testing

--- drizzle/0003_workable_the_watchers.sql
+++
@@ -1,7 +0,0 @@
-ALTER TYPE "public"."section-content-type" RENAME TO "sectionContentType";--> statement-breakpoint
-ALTER TABLE "sinta-candidate-portals_candidate" RENAME COLUMN "stage" TO "linkedin";--> statement-breakpoint
-ALTER TABLE "sinta-candidate-portals_section" ADD COLUMN "index" integer NOT NULL;--> statement-breakpoint
-ALTER TABLE "public"."sinta-candidate-portals_section" ALTER COLUMN "contentType" SET DATA TYPE text;--> statement-breakpoint
-DROP TYPE "public"."sectionContentType";--> statement-breakpoint
-CREATE TYPE "public"."sectionContentType" AS ENUM('Notion Editor', 'Link', 'Document', 'notion', 'pdf');--> statement-breakpoint
-ALTER TABLE "public"."sinta-candidate-portals_section" ALTER COLUMN "contentType" SET DATA TYPE "public"."sectionContentType" USING "contentType"::"public"."sectionContentType";
