38d43db17a52c3be6e0623cda4d4704346c44004
msinta <shegowmo@gmail.com>
2024-11-22 17:34:23 -0500

    update

--- src/config/site.ts
+++ src/config/site.ts
@@ -9,7 +9,7 @@ export const siteConfig = {
     description:
         "Sinta Portals simplify your hiring process by bringing everything candidates need into one place. Personalize experiences, track engagement, and collaborate seamlessly—all while saving time and effort. Designed for Recruiters.",
     orgImage:
-        "https://utfs.io/f/4ae0ddb1-4260-46f5-aa7c-70408cc192b9-aadavt.png",
+        "https://utfs.io/f/NyiNiWCHmEc4T1lfaz7YfVkx8K2lrO9P3I5y4osQAwc6eCLv",
     contactEmail: "team@teamsinta.com",
     noReplyEmail: "team@teamsinta.com",
 } as const;
